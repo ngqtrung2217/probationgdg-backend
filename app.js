a9ef8407346dc44da98772655808f15a6be333d0
ngqtrung2217 <nguyentrung22.01.123@gmail.com>
2025-10-29 14:32:37 +0700

    change app.js and www

--- app.js
+++ app.js
@@ -1,20 +1,51 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const { sequelize } = require("./src/models");
+const routes = require("./src/routes");
+const { notFoundHandler, errorHandler } = require("./src/middlewares");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
+// Middlewares
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+app.use(logger("dev"));
+
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: false, limit: "10mb" }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(express.static(path.join(__dirname, "public")));
+
+// DATABASE CONNECTION
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Database connected successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
+
+// Routes
+app.use("/api", routes);
+
+// 404 Handler
+app.use(notFoundHandler);
+
+// Error Handler
+app.use(errorHandler);
 
 module.exports = app;
